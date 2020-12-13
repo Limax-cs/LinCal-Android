c6b9f9183c9d5e5d3890bd28adeff2b7ef3b6359
Limax <limax10000100100000@gmail.com>
2020-12-13 23:36:12 +0100

    import problemes

--- app/src/main/java/com/example/lincal_android/MainActivity.java
+++ app/src/main/java/com/example/lincal_android/MainActivity.java
@@ -1,20 +1,20 @@
 package com.example.lincal_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.Manifest;
-import android.os.StrictMode;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.content.pm.PackageManager;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
